f608d7e93b6dc4c2a2a3b0ba6d760e0f2d247201
GabrieleLapischies <129321703+GabrieleLapischies@users.noreply.github.com>
2023-05-12 10:04:52 -0300

    Add files via upload

---
+++ prof/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+</head>
+<body>
+    <header>
+        <div>
+            <p>Sistema de cadastro de Professores</p>
+        </div>
+    </header>
+    <nav class="naveg">
+            <div>
+                <p class="cnaveg"><a href="formulariocadas.php">Cadastrar professor</a></p>
+            </div>
+            <div>
+                <p class="cnaveg"><a href="visualizar_cadastro.php">Visualizar cadastro</a></p>
+            </div>
+            <div>
+                <p class="cnaveg"><a href="index.php">Página inicial</a></p>
+            </div>
+    </nav>
+    <section class="sect">
+    <div class="conteudo">
+        <p class="p1">Bienvenidos a la pagina principal</p>
+    </div>
+    </section>
+    
+</body>
+</html>
